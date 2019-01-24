f056a93f3ca17849a6bf54ebb5db49948e27742b
victorpinto <mail@victorpinto.net>
2019-01-24 12:25:52 -0500

    victor added animation thing
    animation system to trigger animations in dialogue

---
+++ Assets/Scripts/AnimationSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity.Example
+{
+
+    public class AnimationSystem : MonoBehaviour
+    {
+        //This struct is how we will organize our animation clips 
+        [System.Serializable]
+        public struct AnimationInfo
+        {
+            public string name;
+            public Animator yarnAnim; //I dont know if we are going to use animation or animation clip here
+              
+        }
+
+
+        public AnimationInfo[] animations;
+
+        // Create a command to play and change animations 
+        [YarnCommand("cueanim")] 
+        public void PlayYarnAnim(string animName)
+        {
+            Debug.Log("YARN COMMAND ACTIVATED!");
+            Animator a = null;
+            foreach(var info in animations) 
+            {
+                if (info.name == animName)
+                {
+                    a = info.yarnAnim;
+                    a.SetTrigger(animName);
+                    Debug.Log("Set Trigger debug");
+
+                    break;
+                }
+            }
+        
+            if (a == null)
+            {
+                Debug.LogErrorFormat("Can't find animator named {0}!", animName);
+                return;
+            }
+
+            a = GetComponent<Animator>();
+        }
+
+
+    }
+}
