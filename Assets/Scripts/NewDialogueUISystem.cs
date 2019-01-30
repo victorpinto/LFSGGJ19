ef54125fa1ba7d849445c7ffcb74d15efb6fc438
victorpinto <mail@victorpinto.net>
2019-01-30 09:57:05 -0500

    Victor Changed things
    changed things

---
+++ Assets/Scripts/NewDialogueUISystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Linq;
+using TMPro;
+
+namespace Yarn.Unity.Example
+{
+
+}
